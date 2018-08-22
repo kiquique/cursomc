387f692c470fb5422411cf95fb962be3258f085a
kiquique <luisjoga1000@gmail.com>
2018-08-22 16:41:40 -0300

    Projeto criado

---
+++ src/main/java/br/com/effect4/aprendendo/AprendendoApplication.java
@@ -0,0 +1,12 @@
+package br.com.effect4.aprendendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoApplication.class, args);
+	}
+}
