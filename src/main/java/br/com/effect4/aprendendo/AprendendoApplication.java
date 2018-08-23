2d8dba89937e29560c897c42a3e1d1e9ad66ecac
kiquique <luisjoga1000@gmail.com>
2018-08-23 13:42:55 -0300

    Criando Operacao de Instanciacao

--- src/main/java/br/com/effect4/aprendendo/AprendendoApplication.java
+++ src/main/java/br/com/effect4/aprendendo/AprendendoApplication.java
@@ -1,12 +1,30 @@
 package br.com.effect4.aprendendo;
 
+import br.com.effect4.aprendendo.domain.Categoria;
+import br.com.effect4.aprendendo.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AprendendoApplication {
+import java.util.Arrays;
 
-	public static void main(String[] args) {
-		SpringApplication.run(AprendendoApplication.class, args);
+@SpringBootApplication
+public class AprendendoApplication implements CommandLineRunner {
+
+	@Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(AprendendoApplication.class, args);
+    }
+
+	@Override
+	public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Desenvolvimento");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
